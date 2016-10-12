912711b1fafc5510e3153845eb71174f5520c185
Chad <chad@chadmott.com>
2016-10-12 11:34:47 +0200

    all the step 4 stuff

--- index.js
+++ index.js
@@ -1,75 +1,9 @@
 'use strict';
-var AWS = require('aws-sdk');
-var Record = (function () {
-    function Record(event, context) {
-        this.event = event;
-        this.context = context;
-        this.HostedZoneId = 'xyz';
-        this.fqdn = 'xyz';
-        this.requestIP = event.sourceIP;
-        this.route53 = new AWS.Route53();
-    }
-    Record.prototype.getRecordIP = function () {
-        var _this = this;
-        var params = {
-            HostedZoneId: this.HostedZoneId,
-            StartRecordName: this.fqdn,
-            StartRecordType: 'A',
-            MaxItems: '1'
-        };
-        this.route53.listResourceRecordSets(params, function (err, data) {
-            if (err) {
-                _this.context.succeed({ update: false, piip: _this.event.sourceIP, reason: 'Route 53 failure' + err });
-            }
-            else {
-                var recordIP = data.ResourceRecordSets[0].ResourceRecords[0].Value;
-                if (_this.requestIP != data.ResourceRecordSets[0].ResourceRecords[0].Value) {
-                    _this.updateRecord();
-                }
-                else {
-                    _this.context.succeed({ update: false, piip: _this.event.sourceIP, reason: 'Request and record are the same' });
-                }
-            }
-        });
-    };
-    ;
-    Record.prototype.updateRecord = function () {
-        var _this = this;
-        var params = {
-            ChangeBatch: {
-                Changes: [
-                    {
-                        Action: 'UPSERT',
-                        ResourceRecordSet: {
-                            Name: this.fqdn,
-                            Type: 'A',
-                            ResourceRecords: [
-                                {
-                                    Value: this.event.sourceIP
-                                },
-                            ],
-                            TTL: 0
-                        }
-                    },
-                ]
-            },
-            HostedZoneId: this.HostedZoneId
-        };
-        this.route53.changeResourceRecordSets(params, function (err, data) {
-            if (err) {
-                _this.context.succeed({ update: false, piip: _this.event.sourceIP, reason: 'Route 53 failure' + err });
-            }
-            else {
-                _this.context.succeed({ update: true, piip: _this.event.sourceIP, reason: 'authenticated and needed update' });
-            }
-        });
-    };
-    return Record;
-}());
+var record_1 = require('./record');
 function handler(event, context) {
     var authcode = 'xyz';
     if (event.authcode === authcode) {
-        var record = new Record(event, context);
+        var record = new record_1.Record(event, context);
         record.getRecordIP();
     }
     else {
--- index.ts
+++ index.ts
@@ -1,82 +1,10 @@
 'use strict';
 
 import * as AWS from 'aws-sdk';
- interface IRecord {
-    HostedZoneId: string;
-    fqdn: string;
-    requestIP: string;
-    route53: any;
-    event: any;
-}
-class Record implements IRecord {
-    HostedZoneId;
-    fqdn;
-    requestIP;
-    route53;
-
-    constructor(public event: any, public context: any) {
-        this.HostedZoneId = 'xyz'; //hosted zone ID from Route53
-        this.fqdn = 'xyz'; //domain name for API
-        this.requestIP = event.sourceIP;
-        this.route53 = new AWS.Route53();
-    }
-    getRecordIP() {
-        var params = {
-            HostedZoneId: this.HostedZoneId, /* required */
-            StartRecordName: this.fqdn,
-            StartRecordType: 'A',
-            MaxItems: '1'
-        };
-
-        this.route53.listResourceRecordSets(params, (err: any, data: any) => {
-            if (err) {
-                this.context.succeed({ update: false, piip: this.event.sourceIP, reason: 'Route 53 failure' + err });
-            }
-            else {
-                var recordIP = data.ResourceRecordSets[0].ResourceRecords[0].Value;
-                if (this.requestIP != data.ResourceRecordSets[0].ResourceRecords[0].Value) {
-                    this.updateRecord();
-                } else {
-                    this.context.succeed({ update: false, piip: this.event.sourceIP, reason: 'Request and record are the same' });
-                }
-            }
-        });
-    };
-    updateRecord() {
-        var params = {
-            ChangeBatch: {
-                Changes: [
-                    {
-                        Action: 'UPSERT',
-                        ResourceRecordSet: {
-                            Name: this.fqdn,
-                            Type: 'A',
-                            ResourceRecords: [
-                                {
-                                    Value: this.event.sourceIP
-                                },
-                            ],
-                            TTL: 0,
-                        }
-                    },
-                ],
-            },
-            HostedZoneId: this.HostedZoneId
-        };
-        this.route53.changeResourceRecordSets(params, (err: any, data: any) => {
-            if (err) {
-                this.context.succeed({ update: false, piip: this.event.sourceIP, reason: 'Route 53 failure' + err });
-            }
-            else {
-                this.context.succeed({ update: true, piip: this.event.sourceIP, reason: 'authenticated and needed update' });
-            }
-        });
-    }
-}
+import {Record} from './record';
 
 export function handler(event: any, context: any) {
-    //config variables
-    const authcode: string = 'xyz'; //rudamentary authentication -- you pass t`his as a get param 
+    const authcode: string = 'xyz'; // rudamentary authentication -- you pass t`his as a get param 
 
     if (event.authcode === authcode) {
         let record = new Record(event, context);
